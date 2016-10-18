a1e2c6e4263cb4937d0976f71c5223771ec57153
Markus Binsteiner <makkus@gmail.com>
2016-10-18 15:50:04 +1300

    Deleted old spacemacs config

--- spacemacs/.spacemacs.d/layers/edit/keybindings.el
+++
@@ -1,4 +0,0 @@
-(global-set-key (kbd "C-M-c") 'makkus/comment-or-uncomment-region-or-line)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "M-C-s") 'anzu-query-replace)
-(global-set-key (kbd "C-j") 'er/expand-region)
--- spacemacs/.spacemacs.d/layers/windows/keybindings.el
+++
@@ -1,4 +0,0 @@
-(global-set-key (kbd "C-<") 'winner-undo)
-(global-set-key (kbd "C->") 'winner-redo)
-(global-set-key (kbd "C-q") 'delete-other-windows)
-(global-set-key (kbd "<f11>")  'makkus/full-screen-toggle)
