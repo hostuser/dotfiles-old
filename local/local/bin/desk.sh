b16b765e2061bd136c7d4f1f82e9a7901c83a245
Markus Binsteiner <makkus@gmail.com>
2016-12-21 10:48:02 +1300

    Desktop monitor setup

--- local/local/bin/desk.sh
+++ local/local/bin/desk.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xrandr --output DP3 --off --output DP2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 2560x344 --rotate normal --output VGA1 --off
+xrandr --output VGA-1 --off --output LVDS-1 --primary --mode 1366x768 --pos 2560x432 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output HDMI-
 
 ~/local/bin/switch-workspace.py 1 DP2
 ~/local/bin/switch-workspace.py 2 DP2
